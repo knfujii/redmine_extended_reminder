6839f4a2f5c4215766372607d44fb90b9375cb85
u8451 <kuninori-fujii@act-systems.co.jp>
2020-03-24 14:07:01 +0900

    fix: compatibility with Redmine 4.x

--- lib/redmine_extended_reminder/mailer_model_patch.rb
+++ lib/redmine_extended_reminder/mailer_model_patch.rb
@@ -2,49 +2,40 @@ require_dependency 'mailer'
 
 module RedmineExtendedReminder
   module MailerModelPatch
-    def self.included(base)
+    def self.prepended(base)
       base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
 
       base.class_eval do
         # replace class methods
         helper :extended_reminder
-        class << self
-          alias_method_chain :reminders, :patch
-        end
-
-        # replace instance methods
-        alias_method_chain :reminder, :patch
       end
     end
-  end
 
-  module ClassMethods
-    def reminders_with_patch(options={})
-      # Non-working days setting is supported in Redmine 2.2 or higher.
-      if Redmine::VERSION.to_a[0..1].join('.').to_f >= 2.2
-        if Setting.plugin_redmine_extended_reminder[:disable_on_non_woking_days].to_i != 0
-          date_calc = Object.new
-          date_calc.extend Redmine::Utils::DateCalculation
-          return nil if date_calc.non_working_week_days.include?(Date.today.wday)
+    module ClassMethods
+      def reminders(options={})
+        # Non-working days setting is supported in Redmine 2.2 or higher.
+        if Redmine::VERSION.to_a[0..1].join('.').to_f >= 2.2
+          if Setting.plugin_redmine_extended_reminder[:disable_on_non_woking_days].to_i != 0
+            date_calc = Object.new
+            date_calc.extend Redmine::Utils::DateCalculation
+            return nil if date_calc.non_working_week_days.include?(Date.today.wday)
+          end
         end
+
+        days_override = Setting.plugin_redmine_extended_reminder[:days].to_i
+        options[:days] = days_override if days_override > 0
+        super(options)
       end
-
-      days_override = Setting.plugin_redmine_extended_reminder[:days].to_i
-      options[:days] = days_override if days_override > 0
-      reminders_without_patch(options)
     end
-  end
 
-  module InstanceMethods
-    def reminder_with_patch(user, issues, days)
+    def reminder(user, issues, days)
       saved_status = ActionMailer::Base.perform_deliveries
       if user.pref[:extended_reminder_no_reminders]
         ActionMailer::Base.perform_deliveries = false
       end
       @issues_by_date = issues.sort_by(&:due_date).group_by(&:due_date)
       @count = issues.size
-      reminder_without_patch(user, issues, days)
+      super(user, issues, days)
     ensure
       ActionMailer::Base.perform_deliveries = saved_status
     end
@@ -52,5 +43,5 @@ module RedmineExtendedReminder
 end
 
 unless Mailer.included_modules.include?(RedmineExtendedReminder::MailerModelPatch)
-  Mailer.send(:include, RedmineExtendedReminder::MailerModelPatch)
+  Mailer.send(:prepend, RedmineExtendedReminder::MailerModelPatch)
 end
